68a79f0de232d7f3e38b26193719b091f0998271
Varun <vpsingh1303@gmail.com>
2023-05-14 23:16:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const[data,setData]=useState('')
+  const getApi = () => {
+      axios.get('https://catfact.ninja/fact').then((response) => {
+        console.log(response.data.fact);
+      setData(response.data.fact);
+      }).catch(err =>{console.log(err);})
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={getApi} className='btn'>Fetch Api/Refresh</button>
+    {data && <p>{data}</p>}
     </div>
   );
 }
